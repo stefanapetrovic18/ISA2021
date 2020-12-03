8f37f10e5f0779c5ce8196739a7047dbc2b3f26c
Stefana Petrović <stefanapetrovic18@uns.ac.rs>
2020-12-03 12:12:19 +0100

    Back-end aplikacija (Spring Initializr)

---
+++ apoteka/src/main/java/rs/apoteka/ApotekaApplication.java
@@ -0,0 +1,13 @@
+package rs.apoteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApotekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApotekaApplication.class, args);
+	}
+
+}
