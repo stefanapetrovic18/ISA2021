51d363845400bed607ba1acf27a805c7872e0001
Stefana Petrović <stefanapetrovic18@uns.ac.rs>
2021-02-12 18:52:26 +0100

    Spring security - CORS filter za front, zasticeni endpointi

--- apoteka/src/main/java/rs/apoteka/security/SecurityConfiguration.java
+++ apoteka/src/main/java/rs/apoteka/security/SecurityConfiguration.java
@@ -13,9 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import rs.apoteka.auth.JWTAuthEntryPoint;
 import rs.apoteka.auth.JWTAuthTokenFilter;
 import rs.apoteka.service.impl.auth.UserDetailsServiceImpl;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -47,16 +53,50 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    //TODO: Cors filter za front
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "OPTIONS", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "AuthToken"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        //TODO: Zastititi pristup endpointima
+//        http.cors().and().csrf().disable()
+//                .authorizeRequests().antMatchers("/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        //TODO: Zastititi pristup endpointima
-        http.cors().and().csrf().disable()
-                .authorizeRequests().antMatchers("/**").permitAll()
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors().and().csrf().ignoringAntMatchers("/api/user/signin").disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/api/user/login",
+                        "/api/user/register",
+                        "/api/user/confirm",
+                        "/api/user/request-token",
+                        "/api/user/change-password",
+                        "/api/medicine",
+                        "/api/pharmacy"
+                ).permitAll()
                 .anyRequest().authenticated()
-                .and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                .and().headers().cacheControl().disable()
+                .and().requestCache().disable();
+
+        httpSecurity.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
